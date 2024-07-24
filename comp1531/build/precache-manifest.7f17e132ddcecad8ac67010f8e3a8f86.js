3128a20cf2889cafaba1ada478b841f6fe6af760
David <z5359104@ad.unsw.edu.au>
2024-07-24 18:48:20 +1000

    Added to repo

---
+++ comp1531/build/precache-manifest.7f17e132ddcecad8ac67010f8e3a8f86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b582b99a354eee50fdac8dd67c92e1cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08662f6af443573229be",
+    "url": "/static/css/2.352a0142.chunk.css"
+  },
+  {
+    "revision": "f14fb5e34cbcaf4b264c",
+    "url": "/static/css/main.af341aa7.chunk.css"
+  },
+  {
+    "revision": "08662f6af443573229be",
+    "url": "/static/js/2.ffd41df9.chunk.js"
+  },
+  {
+    "revision": "f14fb5e34cbcaf4b264c",
+    "url": "/static/js/main.13cf5db6.chunk.js"
+  },
+  {
+    "revision": "a2806f7ab0fa17a92097",
+    "url": "/static/js/runtime~main.dc128e9d.js"
+  }
+]);
--- src/error.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.exceptions import HTTPException
-
-class AccessError(HTTPException):
-    code = 403
-    message = 'Access Error'
-
-class InputError(HTTPException):
-    code = 400
-    message = 'Input Error'
